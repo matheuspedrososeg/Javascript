106675b5e23498c593e08235b90580e1f8931bf9
Matheus Pedroso <matheus.pedroso@NOTESEG-DEV15.segalas.local>
2025-01-07 08:40:23 -0300

    testing game

---
+++ EventListenerTest/index.js
@@ -0,0 +1,34 @@
+
+
+document.addEventListener("keydown", function (event) {
+    movePlayer(event.key)
+});
+
+function movePlayer(key) {
+    var player = document.getElementsByClassName("player")[0];
+
+    var bottomMargin = parseInt(player.style.marginBottom || 0);
+    var topMargin = parseInt(player.style.marginTop || 0);
+    var leftMargin = parseInt(player.style.marginLeft || 0);
+    var rightMargin = parseInt(player.style.marginRight) || 0;
+
+    switch (key) {
+        case "w":
+            player.style.marginTop = (topMargin - 15) + "px";
+            break;
+
+        case "a":
+            player.style.marginRight = (rightMargin + 15) + "px";
+            break;
+        case "s":
+            player.style.marginTop = (topMargin + 15) + "px";
+            break;
+
+        case "d":
+            player.style.marginLeft = (leftMargin + 15) + "px";
+            break;
+
+        default:
+            break;
+    }
+}
